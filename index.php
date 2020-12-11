7d8062dcc6e34665134c53c374cc261139dc6a4c
louiscmc <62025302+fregaton@users.noreply.github.com>
2020-12-11 14:55:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Binet Love</title>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Bootstrap CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="js/jquery.min.js"></script>
-        <script src="js/popper.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <!-- Mon CSS Perso -->
-        <link href="css/perso.css" rel="stylesheet">
-    </head>
+    <?php requires("utils.php");
+        $title="Binet Love";
+        $CSS="perso.css";
+        generateHTMLHeader($title, $CSS);
+        ?>
     <body>
         <header>
             <nav class="navbar navbar-expand-md navbar-dark navbar-fixed-top bg-pink">
