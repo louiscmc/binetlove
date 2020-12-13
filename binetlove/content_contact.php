62a94b87e3dac94d1053c297e1ed96c50d327f33
mathildeeandre <mtldandre@icloud.com>
2020-12-13 18:00:00 +0100

    moults changements stylés

---
+++ binetlove/content_contact.php
@@ -0,0 +1,4 @@
+<div id="jumbo1" class="container-fluid bg-faded">
+    <br><br><br><br><br><br><br><br><br><br><br>
+</div>
+
---
+++ binetlove/content_welcome.php
@@ -0,0 +1,63 @@
+<main>  
+            <div id="jumbo1" class="jumbotron jumbotron-fluid bg-faded">
+              <div class="container">
+                  <h1 class="display-4 text-focus-in">Spread <span class='red'>love</span>, not the virus</h1>
+              </div>
+            </div>
+            <div id="myCarousel" class="carousel">
+                <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+                  <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item">
+                      <img class="d-block w-100" src="images/pulllove.jpg">
+                    <div class="container">
+                      <div class="carousel-caption text-left">
+                        <h1>Example headline.</h1>
+                        <p>Texte random.</p>
+                        <p><a class="btn btn-lg btn-rose" href="index.php?page=design" role="button">Envoyer un design de lettre</a></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="carousel-item active">
+                      <img class="d-block w-100" src="images/lettrelove.jpg">
+                    <div class="container">
+                      <div class="carousel-caption">
+                        <h1>Another example headline.</h1>
+                        <p>Crédits : <a href="https://photo.binets.fr"> binet photo </a></p>
+                        <p><a class="btn btn-lg btn-rose heartbeat" href="index.php?page=letter" role="button">Envoie ta lettre</a></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                      <img class="d-block w-100" src="images/chatnoir.jpg">
+                    <div class="container">
+                      <div class="carousel-caption text-right">
+                        <h1>Prochains événements bientôt !</h1>
+                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                        <p><a class="btn btn-lg btn-rose" href="#" role="button">Browse gallery</a></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+            <div id="jumbo2" class="jumbotron jumbotron-fluid bg-faded">
+                <div class="container">
+                    <h1 class="display-4">Il est temps d'écrire une lettre !</h1>
+                    <p class="lead">N'oubliez pas qu'il sera possible d'écrire des lettres directement sur papier aux prochains événements.</p>
+                    <hr class="my-4">
+                    <p>Envoyez tout votre amour ! &#128149;</p>
+                    <a class="btn btn-rose btn-lg" href="index.php?page=letter" role="button">Ecrire une lettre</a>
+                </div>
+            </div>
+        </main>
