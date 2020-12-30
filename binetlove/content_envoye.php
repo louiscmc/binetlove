3820c16cdcc18f45188f99986966f8749e5e2cf9
mathildeeandre <mtldandre@icloud.com>
2020-12-30 09:12:20 +0100

    mini typo (mais sinon t'es incroyable :O)

--- binetlove/content_envoye.php
+++ binetlove/content_envoye.php
@@ -1,6 +1,6 @@
 <div class="container">
     <br>
-    <a class="btn btn-light float-right" href="index.php?page=lettre" role="button">Revenir au formulaire</a>
+    <a class="btn btn-light float-right" href="index.php?page=letter" role="button">Revenir au formulaire</a>
     <?php get_lettres($dbh, $_SESSION['login']); ?>
 </div>
 
