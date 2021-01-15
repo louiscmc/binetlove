4759b228f90b726c33e09ff0699eaa91312d8b1a
louiscmc <62025302+fregaton@users.noreply.github.com>
2021-01-15 15:16:00 +0100

    Update utilisateur.php

--- binetlove/classes/utilisateur.php
+++ binetlove/classes/utilisateur.php
@@ -1,7 +1,31 @@
 <?php
-function getUser($dbh,$login){
+
+class Utilisateur {
+    public $login;
+    public $nom;
+    public $prenom;
+    public $password;
     
+    public function __toString() {
+        return "[{$this->login}] {$this->prenom}";
+    }
+
+    public static function getUser($dbh,$login){
+        $query = "SELECT * FROM polytechniciens WHERE login = ?";
+        $sth = $dbh->prepare($query);
+        $sth->setFetchMode(PDO::FETCH_CLASS, 'Utilisateur');
+        $sth->execute(array($login));
+        if ($user = $sth->fetch()){
+            return $user;
+        }
+        else{
+            return null;
+        }
+    }
 }
+
+
+
 
 function testPassword($dbh,$password){
 
