92d5d673c758dc9d4097f3a18ccbb25deef85951
mathildeeandre <mtldandre@icloud.com>
2021-01-15 14:23:00 +0100

    Update printForms.php

--- binetlove/scripts/printForms.php
+++ binetlove/scripts/printForms.php
@@ -2,30 +2,17 @@
 function printLoginForm($askedPage){
 echo <<<CHAINE_DE_FIN
 <form class="form-inline my-2 my-lg-0" action="index.php?page=$askedPage&todo=login" method="POST">
-<div id="div_login">
-    <h1>Connecte toi !</h1>
-   <div>
-     <input type="text" class="form-control mr-sm-2" name="login" value="" placeholder="Login" />
-   </div>
-   <div>
-     <input  type="password" class="form-control mr-sm-2" name="password" value="" placeholder="Mot de Passe"/>
-   </div>
-   <div>
-     <input  class="btn-rose btn-login my-2 my-sm-0" type="submit" value="Se connecter" name="but_submit" id="but_submit" />
-   </div>
-   <div>
-     <a href='index.php?page=welcome&todo=register'><input class="btn-rose btn-login my-2 my-sm-0" type="submit" value="S'inscrire'" name="but_register" id="but_register" /></a>
-   </div>
-</div>
+     <input type="text" class="form-control mr-sm-1" name="login" value="" placeholder="Login" />
+     <input  type="password" class="form-control mr-sm-1" name="password" value="" placeholder="Mot de Passe"/>
+     <input  class="btn btn-rose my-1 my-sm-0" type="submit" value="Se connecter" name="but_submit" id="but_submit" />
+     <a href='index.php?page=welcome&todo=register'><input class="btn btn-rose my-1 my-sm-0" type="submit" value="S'inscrire" name="but_register" id="but_register" /></a>
 </form>
 CHAINE_DE_FIN;
 }
 
 function printLogoutForm($askedPage){
 echo<<<CHAINE_DE_FIN
-<div style='text-align: right'>
 <a href='index.php?page=welcome&todo=logout'><input type="submit" value="Se déconnecter" class="btn-rose btn-login my-2 my-sm-0" name="but_logout" id="but_logout"/></a>
-</div>
 CHAINE_DE_FIN;
 }
 
