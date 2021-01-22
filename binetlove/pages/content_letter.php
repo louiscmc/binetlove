507124c4324d3a8f9ea0f83cba6986e8d1b32e0e
louiscmc <62025302+fregaton@users.noreply.github.com>
2021-01-22 18:08:12 +0100

    cwxcwxcwx

--- binetlove/pages/content_letter.php
+++ binetlove/pages/content_letter.php
@@ -109,8 +109,24 @@
                             </div>
                     </div>
                     <div class="col-md-3">
-                    
-                        
+                        <div class="container vertical-scrollable">  
+                            <div class="row text-center">  
+                                <div class="col-sm-8"> 
+                                1</div>  
+                                <div class="col-sm-8"> 
+                                2</div>  
+                                <div class="col-sm-8"> 
+                                3</div>  
+                                <div class="col-sm-8"> 
+                                4</div>  
+                                <div class="col-sm-8"> 
+                                5</div>  
+                                <div class="col-sm-8"> 
+                                6</div>  
+                                <div class="col-sm-8"> 
+                                7</div>  
+                            </div>  
+                        </div>  
                     </div>
                 </div>
                 <div class="container-fluid col-md-7 offset-md-1">
