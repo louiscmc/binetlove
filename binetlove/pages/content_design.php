224280ac32baa5a55603bbdb0ae771cd5a279b45
mathildeeandre <mtldandre@icloud.com>
2021-01-22 16:21:07 +0100

    omnu

--- binetlove/pages/content_design.php
+++ binetlove/pages/content_design.php
@@ -1,15 +1,8 @@
 <div id="jumbo1" class="container-fluid bg-faded">
-<br>
-<br>
-<div class="row">
-<div class="col-md-4 offset-md-4">
-<form action="upload.php" method="post" enctype="multipart/form-data">
-   <input type="file" name="fichier"/>
-   <br>
-   <input type="submit" value="envoyer" />
-</form>
-<br>
-<br>
-</div>
-</div>
+   <div class="row">
+      <form method='post' action='upload.php' enctype='multipart/form-data'>
+      <input type='file' name='files[]' multiple />
+      <input type='submit' value='Submit' name='submit' />
+      </form>
+   </div>
 </div>
