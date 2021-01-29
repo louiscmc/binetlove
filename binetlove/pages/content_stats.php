884896100f12acfd6a544dbb5ee9377331347e0e
mathildeeandre <mtldandre@icloud.com>
2021-01-29 17:50:42 +0100

    Update content_stats.php

--- binetlove/pages/content_stats.php
+++ binetlove/pages/content_stats.php
@@ -1,4 +1,4 @@
-<div id="jumbo1" class="container-fluid bg-faded">
+<div id="jumbo1" class="jumbotron jumbotron-fluid bg-faded">
     <div class="container">
         <h1 class="display-4 text-focus-in">Voici quelques <span class='red'>statistiques</span> !</h1>
     </div>
