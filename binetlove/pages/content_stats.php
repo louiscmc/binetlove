5ad8a86527a27ec7c4be02ab9f743025417c7713
louiscmc <62025302+fregaton@users.noreply.github.com>
2021-01-30 17:03:09 +0100

    ZOOM WALA

--- binetlove/pages/content_stats.php
+++ binetlove/pages/content_stats.php
@@ -6,14 +6,20 @@
         <h1 class="display-4 text-focus-in">Voici quelques <span class='red'>statistiques</span> !</h1>
     </div>
 </div>
-<div class='container'>
-    <div class="ct-chart ct-golden-section"></div>  
+<div class='container chart'>
+    <div class="ct-chart ct-major-sixth" id='timeline'></div> 
 </div>
+<br>
+<br>
+
 
 <!-- scripts pour graphes -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/luxon@1.25.0/build/global/luxon.min.js"></script>
 <script src="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
+<script src="js/chartist-plugin-axistitle.js"></script>
+<script src="js/chartist-plugin-zoom.js"></script>
+
 <script>
     // On récupère les données de la DB grâce à la fonction timeline de database.php qui renvoie un JSON
     var data_ini = '<?php timeline($dbh); ?>';
@@ -47,14 +53,56 @@
 
     // On donne les options pour que ça formate bien (espacement des dates conforme)
     var options = {
-  axisX: {
-    type: Chartist.FixedScaleAxis,
-    divisor: 5,
-    labelInterpolationFnc: function(value) {
-      return moment(value).format('MMM D');
-    }
-  }
+        axisX: {
+            type: Chartist.FixedScaleAxis,
+            divisor: 5,
+            labelInterpolationFnc: function(value) {
+                return moment(value).format('MMM D');
+            }
+        },
+        plugins: [
+            Chartist.plugins.zoom({onZoom: onZoom })
+        ]
+        /* plugins: [
+            Chartist.plugins.ctAxisTitle({
+                axisX: {
+                axisTitle: 'Temps',
+                axisClass: 'ct-axis-title',
+                offset: {
+                    x: 0,
+                    y: 50
+                },
+                textAnchor: 'middle'
+                },
+                axisY: {
+                axisTitle: 'Y title',
+                axisClass: 'ct-axis-title',
+                offset: {
+                    x: 0,
+                    y: 0
+                },
+                textAnchor: 'middle',
+                flipTitle: false
+                }
+            })
+        ] */
 };
 
-    new Chartist.Line('.ct-chart', data, options);
+    var chart = Chartist.Line('.ct-chart', data, options);
+    var resetFnc;
+    function onZoom(chart, reset) {
+        resetFnc = reset;
+    }
+
+    var btn = document.createElement('button');
+    btn.id = 'reset-zoom-btn';
+    btn.classList.add("btn");
+    btn.classList.add("btn-rose");
+    btn.innerHTML = 'Dézoomer';
+    btn.style.float = 'right';
+    btn.addEventListener('click', function() {
+        resetFnc && resetFnc();
+    });
+    var parent = document.querySelector('.chart');
+    !parent.querySelector('#reset-zoom-btn') && parent.appendChild(btn);
 </script>
