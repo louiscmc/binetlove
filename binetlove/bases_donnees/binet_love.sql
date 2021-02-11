095e225f4abcfbc2e283ef16e96d4851fdb73504
mathildeeandre <mtldandre@icloud.com>
2021-02-12 00:06:55 +0100

    TVX

--- binetlove/bases_donnees/binet_love.sql
+++ binetlove/bases_donnees/binet_love.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  jeu. 11 fév. 2021 à 21:28
--- Version du serveur :  10.4.8-MariaDB
--- Version de PHP :  7.3.11
+-- Hôte : localhost
+-- Généré le : ven. 12 fév. 2021 à 00:00
+-- Version du serveur :  10.4.17-MariaDB
+-- Version de PHP : 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `binet_love`
+-- Base de données : `binet_love`
 --
 
 -- --------------------------------------------------------
@@ -85,7 +84,15 @@ INSERT INTO `lettre` (`id`, `login`, `destinataire`, `contenu`, `design`, `chupa
 (57, 'louiscmc', 'louis_vaneau', '&lt;p&gt;ouin ouin le covid&lt;/p&gt;', 'upload/design4', 1, '2021-01-31 21:18:47', 0),
 (59, 'louiscmc', 'louiscmc', '&lt;p&gt;efdffs&lt;span style=&quot;font-family:Fine College&quot;&gt;fdsfsfdsds&lt;/span&gt;&lt;/p&gt;', 'upload/design3', 1, '2021-02-06 15:06:06', 0),
 (60, 'louiscmc', 'louis_vaneau', '&lt;p&gt;sdqsdsqd&lt;/p&gt;', 'upload/design4', 0, '2021-02-06 15:06:24', 0),
-(61, 'louiscmc', 'mathildea', '&lt;p&gt;sdfsdfsdf&lt;/p&gt;', 'upload/design1', 1, '2021-02-11 21:23:17', 0);
+(61, 'louiscmc', 'mathildea', '&lt;p&gt;sdfsdfsdf&lt;/p&gt;', 'upload/design1', 1, '2021-02-11 21:23:17', 0),
+(62, 'mathildea', 'louiscmc', '&lt;p&gt;qzfsnmoi&lt;/p&gt;', 'upload/design1', 0, '2021-02-11 22:32:17', 0),
+(63, 'mathildea', 'louiscmc', '&lt;p&gt;lseixjm&lt;/p&gt;', 'upload/design1', 0, '2021-02-11 22:32:38', 0),
+(64, 'mathildea', 'louiscmc', '&lt;p&gt;lseixjm&lt;/p&gt;', 'upload/design1', 0, '2021-02-11 22:34:34', 0),
+(65, 'mathildea', 'louiscmc', '&lt;p&gt;kjdvnw&amp;lt;m&amp;ugrave;op&lt;/p&gt;', 'upload/design1', 0, '2021-02-11 22:35:29', 0),
+(66, 'mathildea', 'louiscmc', '&lt;p&gt;kjdvnw&amp;lt;m&amp;ugrave;op&lt;/p&gt;', 'upload/design1', 0, '2021-02-11 22:35:40', 0),
+(67, 'mathildea', 'louiscmc', '&lt;p&gt;&amp;ugrave;poezis;:&amp;ugrave;&lt;/p&gt;', 'upload/design1', 0, '2021-02-11 22:35:48', 0),
+(68, 'mathildea', 'louiscmc', '&lt;p&gt;msdjlkbm&lt;/p&gt;', 'upload/design1', 0, '2021-02-11 22:52:16', 0),
+(69, 'mathildea', 'louis_vaneau', '&lt;p&gt;s&amp;lt;evtrb&amp;sect;qest&amp;egrave;nudy,tifu;oppo,inurbyetvzrcaevtbyqnru,io;rp;o!,i&amp;egrave;nubyvt&amp;#39;r&amp;quot;c&amp;eacute;er&amp;quot;vt&amp;#39;by(nui,ouriptouori,eyunybetzvarcEVTRIYU&lt;/p&gt;', 'upload/design1', 1, '2021-02-11 22:55:23', 0);
 
 -- --------------------------------------------------------
 
@@ -102,17 +109,18 @@ CREATE TABLE `polytechniciens` (
   `prenom` varchar(64) NOT NULL,
   `section` varchar(32) NOT NULL,
   `promotion` int(11) NOT NULL,
-  `casert` int(11) NOT NULL
+  `casert` int(11) NOT NULL,
+  `gagnant` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `polytechniciens`
 --
 
-INSERT INTO `polytechniciens` (`id`, `login`, `admin`, `password`, `nom`, `prenom`, `section`, `promotion`, `casert`) VALUES
-(0, 'louiscmc', 1, 'd82ece8d514aca7e24d3fc11fbb8dada57f2966c', 'Cattin--Mota de Campos', 'Louis', 'Natation', 2019, 702013),
-(1, 'mathildea', 1, 'e05bfbc4670d242fdf5e9512e408adb7df517863', 'André', 'Mathilde', 'Escalade', 2019, 123003),
-(2, 'louis_vaneau', 0, 'pass_vaneau', 'Vaneau', 'Louis', 'Roulade', 1828, 11001);
+INSERT INTO `polytechniciens` (`id`, `login`, `admin`, `password`, `nom`, `prenom`, `section`, `promotion`, `casert`, `gagnant`) VALUES
+(0, 'louiscmc', 1, 'd82ece8d514aca7e24d3fc11fbb8dada57f2966c', 'Cattin--Mota de Campos', 'Louis', 'Natation', 2019, 702013, 0),
+(1, 'mathildea', 1, 'e05bfbc4670d242fdf5e9512e408adb7df517863', 'André', 'Mathilde', 'Escalade', 2019, 123003, 0),
+(2, 'louis_vaneau', 0, 'pass_vaneau', 'Vaneau', 'Louis', 'Roulade', 1828, 11001, 0);
 
 --
 -- Index pour les tables déchargées
@@ -150,7 +158,7 @@ ALTER TABLE `images`
 -- AUTO_INCREMENT pour la table `lettre`
 --
 ALTER TABLE `lettre`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
