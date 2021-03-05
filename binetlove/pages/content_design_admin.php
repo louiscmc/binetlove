6cecdf5e0c177505db64a9a38a349f4c52aedb4a
louiscmc <62025302+fregaton@users.noreply.github.com>
2021-03-05 23:11:55 +0100

    Update content_design_admin.php

--- binetlove/pages/content_design_admin.php
+++ binetlove/pages/content_design_admin.php
@@ -4,78 +4,56 @@
     <br>
     <?php
     $login = $_SESSION['login'];
-    $result = $dbh->prepare("SELECT id, image, selec "
-            . "FROM images ");
+    $result = $dbh->prepare("SELECT id, image, selec, login, time FROM images ");
     $result->execute();
         if ($result->rowCount() > 0){
-                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    //récupération des données 
-                    $id=$row['id'];
-                    $temps=$row['time'];
-                    $time= strftime('%T le %D', strtotime($temps));
-                    $image=$row['image'];
-                    //gestion des images sélectionnées
-                    $selec=$row['selec'];
-                    $dessin_selec="❌";
-                    $css_selec="rose";
-                    $btn_selec="Selectionner cette image !";
-                    if ($selec==1){
-                        $dessin_selec="✅";
-                        $btn_selec="Ne plus sélectionner cette image";
-                        $css_selec="red";
-                    }
-                    // si bouton selectionné
-                    if(array_key_exists('selec'.$stringid, $_POST)) { 
-                        modifierSelec($dbh, $id, $selec);
-                        echo"<script> location = location; </script>";
-                    } 
-                    $contenu=html_entity_decode($contenu);
-                    //var_dump(htmlentities($contenu));
-                    echo <<<carte
-                    <div class="card bg-dark text-white">
-                        <img class="card-img" src="$image" alt="Card image">
-                        <div class="card-img-overlay">
-                            <span class='float-right'>$dessin_selec</span>
-                            <form method='post'> 
-                                <input type='submit' name='selec$id' class='btn btn-$css_selec' value='$btn_selec' /> 
-                            </form> 
-                        </div>
-                    </div>
-                        <div class='row'>
-                            <div class='col-sm'>
-                                <div class='card' >
-                                    <div class='card-header' style="background-image: url('$design'); background-position: center;">
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                //récupération des données 
+                $login=$row['login'];
+                $id=$row['id'];
+                $stringid=strval($id);
+                $temps=$row['time'];
+                $time= strftime('%T le %D', strtotime($temps));
+                $image=$row['image'];
+                //gestion des images sélectionnées
+                $selec=$row['selec'];
+                $dessin_selec="❌";
+                $css_selec="rose";
+                $btn_selec="Selectionner cette image !";
+                if ($selec==1){
+                    $dessin_selec="✅";
+                    $btn_selec="Ne plus sélectionner cette image";
+                    $css_selec="red";
+                }
+                // si bouton selectionné
+                if(array_key_exists('selec'.$stringid, $_POST)) { 
+                    modifierSelec($dbh, $id, $selec);
+                    echo"<script> location = location; </script>";
+                } 
+                echo <<<carte
+                
+                    <div class='row'>
+                        <div class='col-sm'>
+                            <div class="card mb-3">
+                                <img class="card-img-top" src="$image" alt="Card image">
+                                <div class="card-body">
+                                    <span class='float-right'>$dessin_selec</span>
+                                    <form method='post'> 
+                                        <input type='submit' name='selec$id' class='btn btn-$css_selec' value='$btn_selec' /> 
+                                    </form> 
+                                </div>
+                                <div class='card-footer text-muted'>
                                         <span class="card-txt">
-                                            $prenom $nom ($section $promotion)
+                                            Envoyé à $time par $login
                                         </span>
-                                        <span class='float-right'>$dessin_chupa</span>
-                                    </div>
-                                    <div class='card-body'>
-                                        <div>
-                                            $contenu
-                                        </div>
-                                        <br>
-                                        <form method='post'> 
-                                                <input type='submit' name='selec$id' class='btn btn-$css_chupa' value='$btn_chupa' /> 
-                                        </form> 
-                                    </div>
-                                    <div class='card-footer text-muted' style="background-image: url('$design');background-position: bottom;">
-                                    <span class="card-txt">
-                                        Envoyé à $time</span>
-                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <br>
-carte; }
-                    if(array_key_exists('modfinal'.$stringid, $_POST)){
-                        $contenumod=$_POST['contenumod'.$id];
-                        $contenumodok=test_input($contenumod);
-                        modifierLettre($dbh, $id, $contenumodok);
-                        echo"<script> location = location; </script>";
-                    }
-                }
-            } 
+                    </div>
+                    <br>
+carte;              
+            }
+        }
         else {
             echo "
                         <div class='row'>
